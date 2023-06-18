9f2f06773d2c4859561d264c2ef319f4cb4e04a2
Andresc06 <andrescontreras370@gmail.com>
2023-06-18 17:25:16 -0500

    feat: Add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import { HeaderBar } from './components/HeaderBar/HeaderBar'
 
 function App() {
 
   return (
     <>
+    <HeaderBar />
       <h1 >
         Hi there!
       </h1>
