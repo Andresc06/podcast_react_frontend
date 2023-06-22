cb34662ce05da7be6930248cafa9d1aa2abce068
Andresc06 <andrescontreras370@gmail.com>
2023-06-21 19:32:38 -0500

    feat: Add Principal component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { HeaderBar } from './components/HeaderBar/HeaderBar'
+import { Principal } from './components/Principal/Principal'
 
 function App() {
 
   return (
     <>
     <HeaderBar />
+    <Principal />
       <h1 >
         Hi there!
       </h1>
