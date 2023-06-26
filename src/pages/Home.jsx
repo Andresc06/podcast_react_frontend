6180865e03d7b90a400d635f45c480ad97720525
jesus <jesus_lopez1997@hotmail.com>
2023-06-26 11:00:05 -0400

    chore: add About component to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import { Footer } from "../components/Footer/Footer";
 import { HeaderBar } from "../components/HeaderBar/HeaderBar";
 import { Principal } from "../components/Principal/Principal";
 import { Reviews } from "../components/Reviews/Reviews";
+import { About } from "../components/About/About";
 
 export function Home() {
   return (
     <>
       <HeaderBar />
       <Principal />
+      <About />
       <FAQ />
       <Reviews />
       <Footer />
