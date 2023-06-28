72a5d3c1ba00fb800ace0dddeda41e7fbf855211
jesus <jesus_lopez1997@hotmail.com>
2023-06-28 01:53:09 -0400

    add: spotify services

--- src/services/services.js
+++ src/services/services.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
-const API_URL = import.meta.env.VITE_BACKEND_URL;
 
 async function registerUser(form) {
+    const API_URL = import.meta.env.VITE_BACKEND_URL;
     try {
         const request_url = new URL(`${API_URL}/create_user?name=${form.name}&email=${form.email}&phone=${form.phone}${form.description ? `&description=${form.description}` : ''}`);
         const response = await axios.post(request_url.href);
@@ -11,4 +11,33 @@ async function registerUser(form) {
     }
 }
 
-export { registerUser }
+async function getSpotifyShows(token) {
+    try {
+        const headers = { Authorization: `Bearer ${token}` }
+        const spotifyEndpoint = 'https://api.spotify.com/v1/me/shows';
+        const response = await axios.get(spotifyEndpoint, { headers })
+        if (response.data.items.length === 0) {
+            return null
+        }
+        const show = response.data.items[0].show;
+        return show;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function getSpotifyUser(token) {
+    try {
+        const headers = { Authorization: `Bearer ${token}` }
+        const spotifyEndpoint = 'https://api.spotify.com/v1/me';
+        const response = await axios.get(spotifyEndpoint, { headers })
+        if (response.status === 200) {
+            return response.data
+        }
+        return null
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export { registerUser, getSpotifyShows, getSpotifyUser }
